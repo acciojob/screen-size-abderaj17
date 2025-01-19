e849a2364cc331e454c9dcf1a2cb50de41532936
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-19 14:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const sizeInfo = document.getElementById('sizeInfo');
+
+const update = ()=>{
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+
+	sizeInfo.querySelector('h1').textContent = `Widht : ${width} and height: ${height}.`;
+}
+
+update();
+
+
+window.addEventListener('resize', update);
